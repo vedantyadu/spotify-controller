48d5a9896c7c9cf7f237839ccc2e187c332aef0d
Vedant Tiwari <vedantyadu@gmail.com>
2023-07-31 01:05:55 +0530

    removed menu

--- main.js
+++ main.js
@@ -21,7 +21,11 @@ function createWindow() {
     }
   })
 
-  // For production - win.loadFile('./dist/index.html')
+  /* 
+    For production - 
+    win.removeMenu()
+    win.loadFile('./dist/index.html') 
+  */
   win.loadURL('http://localhost:5173/')
 }
 
